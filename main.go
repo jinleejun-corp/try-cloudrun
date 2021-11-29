f838f9c3b24cdcf8b5c269995dc175508793c0b3
Best <siraphop.thnthm@gmail.com>
2021-11-29 16:28:09 +0700

    test cobra with port

--- main.go
+++ main.go
@@ -6,24 +6,43 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/spf13/cobra"
 )
 
+var rootCmd = &cobra.Command{
+	Use:     "jinleejun-backend",
+	Short:   "jinleejun",
+	Example: "go run main.go",
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Print("starting server...")
+		http.HandleFunc("/", handler)
+
+		// Determine port for HTTP service.
+		port := os.Getenv("PORT")
+		if port == "" {
+			port = "8080"
+			log.Printf("defaulting to port %s", port)
+		}
+
+		// Start HTTP server.
+		log.Printf("listening on port %s", port)
+		if err := http.ListenAndServe(":"+port, nil); err != nil {
+			log.Fatal(err)
+		}
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
 func main() {
-	log.Print("starting server...")
-	http.HandleFunc("/", handler)
-
-	// Determine port for HTTP service.
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("defaulting to port %s", port)
-	}
-
-	// Start HTTP server.
-	log.Printf("listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
+	Execute()
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
